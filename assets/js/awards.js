a48a48046ef8ba1fc564bd07091ecf9001690cf8
Yoav Rabinovich <yoav.rabinovich@minerva.kgi.edu>
2022-12-07 15:25:22 +0200

    publish commit 93e8053b757621101fe509da891c43ebb25b8639

--- assets/js/awards.js
+++ assets/js/awards.js
@@ -1,4 +1,12 @@
-$("li.tag").on('mouseover',function(){
-    $("li.tag").removeClass('active');//remove from other elements
-    $(this).addClass('active');
-});
+if (window.matchMedia("(pointer: fine)").matches) {
+        $("li.tag").on('mouseover',function(){
+            $("li.tag").removeClass('active');//remove from other elements
+            $(this).addClass('active');
+        });
+    } else {
+        $("li.tag a").contents().unwrap();
+        $("li.tag").on('click',function(){
+            $("li.tag").removeClass('active');//remove from other elements
+            $(this).addClass('active');
+        });
+    }
